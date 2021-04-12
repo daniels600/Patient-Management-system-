57bf441a9e000c4e43a1ad8271d971372daef52a
daniels600 <59796882+daniels600@users.noreply.github.com>
2021-04-12 11:52:20 +0000

    Added remote MySQl

--- config/constants.php
+++ config/constants.php
@@ -1,10 +1,10 @@
 <?php 
 // Data connection credentials 
 
-define("DB_HOST", "localhost");
-define("DB_USER","root");
-define("DB_PASSWORD", getenv('DB_PASSWORD')?? "");
-define("DB_NAME","medico");
+define("DB_HOST", "remotemysql.com");
+define("DB_USER","YFaIKICcTi");
+define("DB_PASSWORD", "jFznwuUgGR");
+define("DB_NAME","YFaIKICcTi");
 
 
 
